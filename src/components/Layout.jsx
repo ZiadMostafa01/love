8102e90253db721493a45f8702b2484efc46f5f1
ZiadMostafa01 <ziadmostafa9866@gmail.com>
2025-11-05 23:36:31 +0200

    update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import FloatingHearts from "./FloatingHearts";
 
 function Layout() {
   return (
-    <div className="relative min-h-screen w-full py-10 flex items-center justify-center bg-gradient-to-b from-[#ff6b81] to-[#ffc0cb] overflow-hidden">
+    <div className="relative min-h-screen w-full px-3 py-10 flex items-center justify-center bg-gradient-to-b from-[#ff6b81] to-[#ffc0cb] overflow-hidden">
       <FloatingHearts />
       <div className="relative z-10 w-full h-full flex items-center justify-center">
         <Outlet /> {/* هنا هيتحط المحتوى اللي بيتغير */}
